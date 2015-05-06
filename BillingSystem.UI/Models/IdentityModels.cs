95b62cbc4e00cc54ae037e593df8162ab316eabc
LucasBebenek <lucas@PersonalWebZone.com>
2015-05-06 08:27:14 -0700

    Introduced new connection string to point to Sql Server instead of local mdb file

--- BillingSystem.UI/Models/IdentityModels.cs
+++ BillingSystem.UI/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace BillingSystem.UI.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("LukaszWorkConnection", throwIfV1Schema: false)
         {
         }
 
