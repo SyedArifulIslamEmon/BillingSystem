651d775acf53858d8b9f384d6b4d2ef919b1bc8a
LucasBebenek <lucas@PersonalWebZone.com>
2015-05-06 12:38:54 -0700

    Introduced Data Annotation refrence into the project. Updated Display Names of PaymentViewModel

--- BillingSystem.BLL/Models/PaymentViewModel.cs
+++ BillingSystem.BLL/Models/PaymentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,15 @@ namespace BillingSystem.BLL.Models
     public class PaymentViewModel
     {
         public string Expense { get; set; }
+        
         public string Description { get; set; }
+
+        [Display(Name = "Monthly Est. Dues")]
         public double MonthlyEstimatedDues { get; set; }
+
+        [Display(Name = "Interest Rate")]
         public double InterestRate { get; set; }
+
         public DateTime DueDate { get; set; }
     }
 }
