7dd9ec1dbd89b843e5fb980f12f75fd08e8bd013
LucasBebenek <lucas@PersonalWebZone.com>
2015-05-06 13:25:00 -0700

    Created 'InsertNewPayment' method in BLL

--- BillingSystem.BLL/Models/PaymentViewModel.cs
+++ BillingSystem.BLL/Models/PaymentViewModel.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using BillingSystem.DAL;
 
 namespace BillingSystem.BLL.Models
 {
     public class PaymentViewModel
     {
+        private DAL.BillingSystemEntities _bsEntities = new BillingSystemEntities();
+
         [Display(Name = "Expense Name")]
         public string Expense { get; set; }
         
@@ -21,5 +24,19 @@ namespace BillingSystem.BLL.Models
 
         [Display(Name = "Payment Due Date")]
         public DateTime DueDate { get; set; }
+
+        public void InsertNewPayment()
+        {
+            var newPayment = new DAL.Payment();
+            newPayment.Description = this.Description;
+            newPayment.DueDate = this.DueDate;
+            newPayment.Expense = this.Expense;
+            newPayment.InterestRate = this.InterestRate;
+            newPayment.MonthlyEstDues = this.MonthlyEstimatedDues;
+            newPayment.Timestamp = DateTime.Now;
+
+            _bsEntities.Payments.Add(newPayment);
+            _bsEntities.SaveChanges();
+        }
     }
 }
