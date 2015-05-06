67d88195d54ad1fbeb67d78e3f9211db005dd9bc
LucasBebenek <lucas@PersonalWebZone.com>
2015-05-06 11:40:27 -0700

    Introduced Payment Controller and Payment ViewModel

---
+++ BillingSystem.UI/Controllers/PaymentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BillingSystem.UI.Models;
+
+namespace BillingSystem.UI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult ManageBilling()
+        {
+            var payments = new List<PaymentViewModel>();
+            return View(payments);
+        }
+    }
+}
---
+++ BillingSystem.UI/Models/PaymentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillingSystem.UI.Models
+{
+    public class PaymentViewModel
+    {
+        public string Expense { get; set; }
+        public string Description { get; set; }
+        public double MonthlyEstimatedDues { get; set; }
+        public double InterestRate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
